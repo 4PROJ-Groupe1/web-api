5aadb14eaa31752599142d791081330ff08694a6
Paul Riviere <paul.riviere@supinfo.com>
2021-03-18 23:09:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb://example-mongodb-svc.mongo.svc.cluster.local:27017/?replicaSet=example-mongodb"
+//const uri = "mongodb://my-user:Supinf0@10.0.30.20:27017/?replicaSet=example-mongodb"
+
+const uri = "mongodb://my-user:Supinf0@example-mongodb-svc.mongo.svc.cluster.local:27017/?replicaSet=example-mongodb"
+
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
