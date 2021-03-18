c868a5db790824220319516c7a6eec9a83a21021
Paul Riviere <paul.riviere@supinfo.com>
2021-03-18 22:41:06 +0100

    Update index.js
    plus de port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://my-user:Supinf0@example-mongodb-svc.mongo.svc.cluster.local:27017?replicaSet=example-mongodb";
+const uri = "mongodb+srv://my-user:Supinf0@example-mongodb-svc.mongo.svc.cluster.local?replicaSet=example-mongodb";
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
