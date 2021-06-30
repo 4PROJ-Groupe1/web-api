3c69f12d827e4379ebdcc9d99087617115b9f6ca
Paul Riviere <paul.riviere@supinfo.com>
2021-06-30 02:02:12 +0200

    routes rayon
    ajout routes infos, ajout item dans rayon, ajout/delete rayon

--- webapi/routes/rayon/metier.js
+++ webapi/routes/rayon/metier.js
@@ -1,1 +1,127 @@
 require('../../db');
+const utils = require("../../Utils");
+const ObjectID = require('mongodb').ObjectID
+
+
+module.exports = {
+    getInfosRayons: async function () {
+        let client = utils.getNewMongoClient();
+        try {
+            await client.connect();
+            const database = client.db("brilliant_market_test");
+            const shelves = database.collection("shelf");
+            var result = await shelves.find().toArray();
+            if (result?.length > 0) {
+                return result;
+            } else {
+                throw new Error("Pas de rayon enregistré en base");
+            }
+        } catch (e) {
+            throw e;
+        } finally {
+            await client.close();
+        }
+    },
+
+    addRayon: async function (nomRayon) {
+        if (nomRayon && nomRayon.trim() != '') {
+            let client = utils.getNewMongoClient();
+            try {
+                await client.connect();
+                const database = client.db("brilliant_market_test");
+                const shelves = database.collection("shelf");
+                var result = await shelves.insertOne({name: nomRayon, items: []});
+                if (result) {
+                    return {"result": "OK"};
+                } else {
+                    throw new Error("Problème ajout");
+                }
+            } catch (e) {
+                throw e;
+            } finally {
+                await client.close();
+            }
+        } else {
+            throw new Error("Pas de nom");
+        }
+    },
+
+    deleteRayon: async function (idRayon) {
+        if (idRayon && idRayon.trim() != '') {
+            let client = utils.getNewMongoClient();
+            try {
+                await client.connect();
+                const database = client.db("brilliant_market_test");
+                const shelves = database.collection("shelf");
+                var result = await shelves.deleteOne({_id: ObjectID(idRayon)});
+                if (result) {
+                    return {"result": "OK"};
+                } else {
+                    throw new Error("Problème suppression");
+                }
+            } catch (e) {
+                throw e;
+            } finally {
+                await client.close();
+            }
+        } else {
+            throw new Error("Pas d'id");
+        }
+    },
+
+    addItemRayon: async function (idRayon, idProduit, idLot, quantite) {
+        let client = utils.getNewMongoClient();
+        try {
+            await client.connect();
+            const database = client.db("brilliant_market_test");
+            const shelves = database.collection("shelf");
+            // On cherche le rayon concerné
+            var result = await shelves.findOne(
+                {"_id": ObjectID(idRayon)}
+            );
+            if (result) {
+                let productPresent = false;
+                let productIndice = 0;
+                for (const item of result?.items) {
+                    if (item?.productId === idProduit) {
+                        productPresent = true;
+                        break;
+                    }
+                    productIndice++
+                }
+                if (!productPresent) {
+                    result?.items?.push({'productId': idProduit, items: [{idLot: idLot, quantity: quantite}]})
+                } else {
+                    // check si produits de ce lot déjà en rayon
+                    let lotPresent = false;
+                    let indiceLot = 0;
+                    for (const item of result?.items[productIndice]) {
+                        if (item?.idLot === idLot) {
+                            lotPresent = true;
+                            break;
+                        }
+                    }
+                    // Produits de ce lot déjà en rayon
+                    if (lotPresent) {
+                        result.items[productIndice].items[indiceLot].quantity = Number(result?.items[productIndice].items[indiceLot].quantity) + quantite;
+                    } else {
+                        result?.items[productIndice]?.items.push({idLot: idLot, quantity: quantite});
+                    }
+                }
+                var resultInsert = await shelves.updateOne({_id: idRayon}, result?.items);
+
+                if (resultInsert) {
+                    return {"result": "OK"};
+                } else {
+                    throw new Error("Problème enregistrement");
+                }
+            } else {
+                throw new Error("id rayon non présent en base");
+            }
+        } catch (e) {
+            throw e;
+        } finally {
+            await client.close();
+        }
+    }
+};
--- webapi/routes/rayon/routes.js
+++ webapi/routes/rayon/routes.js
@@ -5,8 +5,62 @@ const metier = require('./metier');
 
 // @route GET api/rayon/getInfosRayons
 // @access Public
-router.get('/getInfosRayons', (req, res) => {
-    res.send('test route (GET) working')
+router.get('/getInfosRayons', (req, primaryRes) => {
+    metier.getInfosRayons().then(
+        res => {
+            console.log('RES getInfosRayons : ',res);
+            primaryRes.send(res);
+        },
+        err => {
+            console.log("ERROR getInfosRayons", err);
+            primaryRes.status(500).json({"error": err.message});
+        }
+    );
+});
+
+// @route POST api/rayon/addItemRayon
+// @access Public
+router.post('/addItemRayon', (req, primaryRes) => {
+    metier.addItemRayon(req.body.idRayon, req.body.idProduit, req.body.idLot, req.body.quantite).then(
+        res => {
+            console.log('RES addItemRayon : ',res);
+            primaryRes.send(res);
+        },
+        err => {
+            console.log("ERROR addItemRayon", err);
+            primaryRes.status(500).json({"error": err.message});
+        }
+    );
+});
+
+// @route POST api/rayon/addRayon
+// @access Public
+router.post('/addRayon', (req, primaryRes) => {
+    metier.addRayon(req.body.nomRayon).then(
+        res => {
+            console.log('RES addRayon : ',res);
+            primaryRes.send(res);
+        },
+        err => {
+            console.log("ERROR addRayon", err);
+            primaryRes.status(500).json({"error": err.message});
+        }
+    );
+});
+
+// @route POST api/rayon/deleteRayon
+// @access Public
+router.post('/deleteRayon', (req, primaryRes) => {
+    metier.deleteRayon(req.body.idRayon).then(
+        res => {
+            console.log('RES deleteRayon : ',res);
+            primaryRes.send(res);
+        },
+        err => {
+            console.log("ERROR deleteRayon", err);
+            primaryRes.status(500).json({"error": err.message});
+        }
+    );
 });
 
 module.exports = router;
