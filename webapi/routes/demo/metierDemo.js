6da62718bd6b0e37299ee3fca9eeaa5958a2d210
Paul Riviere <paul.riviere@supinfo.com>
2021-07-04 23:11:24 +0200

    fix demo

--- webapi/routes/demo/metierDemo.js
+++ webapi/routes/demo/metierDemo.js
@@ -32,7 +32,7 @@ module.exports = {
     },
 
     cartValidation: async function () {
-        let panier = await this.getPanier();
+        let panier = await this.getPanier().produits;
         let order = {
             "consumerId": "60e1e7686fa4a1d9a9716a4c", // TODO: mettre id utilisateur demo
             "orderDate": new Date(),
