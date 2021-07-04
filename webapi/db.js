57588af0ed55d1a610fd4cae557f63c92ffa7155
Magnard <48336005+Magnard@users.noreply.github.com>
2021-07-04 19:48:06 +0200

    Change connection url for MongoDB

--- webapi/db.js
+++ webapi/db.js
@@ -1,5 +1,7 @@
 //global.uri = "mongodb://my-user:Supinf0@10.0.30.20:27017/?replicaSet=example-mongodb";
 
-global.uri = "mongodb://my-user:Supinf0@example-mongodb-svc.mongo.svc.cluster.local:27017/?replicaSet=example-mongodb";
+//global.uri = "mongodb://my-user:Supinf0@example-mongodb-svc.mongo.svc.cluster.local:27017/?replicaSet=example-mongodb";
+
+global.uri = "mongodb://example-mongodb-0.example-mongodb-svc.mongo.svc.cluster.local:27017,example-mongodb-1.example-mongodb-svc.mongo.svc.cluster.local:27017,example-mongodb-2.example-mongodb-svc.mongo.svc.cluster.local:27017/?replicaSet=example-mongodb";
 
 //global.uri = "mongodb+srv://BackEnd:ezCxkM1gscNRwpuM@cluster0.emrud.mongodb.net/test";
